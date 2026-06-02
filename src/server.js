19151f2e94710b40571e164a81cf2b7d9a655b64
apple <apple@appleui-MacBookAir.local>
2026-06-02 09:14:54 +0900

    feat: route 합치는 중

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { focusRouter } from "./controllers/focusController.js";
+import focusRouter from "./routes/focusRoute.js";
 import { studyRouter } from "./routes/studyRoute.js";
 import studyEmojiRouter from "./routes/emojiRoute.js";
 
