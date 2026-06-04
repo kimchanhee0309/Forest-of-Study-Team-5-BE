2f647d721a5477c9bca1c19ff547a9c48232e828
cari <aa34500388@gmail.com>
2026-06-04 11:29:18 +0900

    feat: 비밀번호 인증, 삭제하기 기능

--- src/routes/studyDetailRoute.js
+++ src/routes/studyDetailRoute.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getStudyDetail } from "../controllers/studyDetailController.js";
+import {
+  getStudyDetail,
+  verifyPassword,
+  deleteStudy,
+} from "../controllers/studyDetailController.js";
 
 const router = express.Router();
 
 router.get("/studies/:studyId", getStudyDetail);
+router.post("/studies/:studyId/verify-password", verifyPassword);
+router.delete("/studies/:studyId", deleteStudy);
 
 export default router;
