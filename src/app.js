d97304dcf243c5d06f175f5f9b12b3a6e0021ee3
전현선 <jeonhyeonseon@jeonhyeonseon-ui-MacBookPro.local>
2026-06-04 14:40:20 +0900

    feat: 메인홈 백엔드 구현 및 git node_module 제거

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import studyRoute from "./routes/studyRoute.js";
-
-const app = express();
-
-app.use(express.json());
-app.use("/", studyRoute);
-
-export default app;
--- src/routes/studyRoute.js
+++ src/routes/studyRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getStudies } from "../controllers/studies";
+import { getStudies } from "../controllers/studies.js";
 
 const router = express.Router();
 
-router.get("/studis", getStudies);
+router.get("/studies", getStudies);
 
 export default router;
