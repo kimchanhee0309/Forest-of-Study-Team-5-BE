7a7e79d07afac0a631b843f66cd39306b03fa7c9
apple <apple@appleui-MacBookAir.local>
2026-06-05 15:16:59 +0900

    feat: 마무리 작업 및 병합

--- src/server.js
+++ src/server.js
@@ -14,7 +14,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.get("/", (req, res) => {
   res.send("서버 작동중!!");
 });
